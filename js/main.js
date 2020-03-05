4ff16e2921ea38c11b81c4fd08613441edaaa6b9
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-03-05 11:32:30 +0100

    27-rel

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $("#microphone").click(function () {
         var nomeInput = $("#message-input").val();
-        $("message-input").val('');
+        $("#message-input").val('');
 
         var messaggio = $('.template .message.sent').clone();
         messaggio.find('.testo-messaggio').text(nomeInput);
