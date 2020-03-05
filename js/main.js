fa2aabffb0d0d163ea3f5226ae4687c3700e8e24
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-03-05 12:58:48 +0100

    28-rel

--- js/main.js
+++ js/main.js
@@ -2,11 +2,20 @@ $(document).ready(function() {
     $("#microphone").click(function () {
         var nomeInput = $("#message-input").val();
         $("#message-input").val('');
+        var timer = time();
 
         var messaggio = $('.template .message.sent').clone();
         messaggio.find('.testo-messaggio').text(nomeInput);
-        messaggio.find('.orario').text('05.05');
+        messaggio.find('.orario').text(timer);
         $(".mss-scroll-bar").append(messaggio);
     });
 
 });
+
+function time() {
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var hoursMinutes = hours + ":" + minutes;
+    return hoursMinutes;
+}
